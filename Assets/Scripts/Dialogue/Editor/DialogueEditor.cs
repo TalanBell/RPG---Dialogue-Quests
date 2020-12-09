e4467e1f79f13d5da43e7e818fe5dfbae1a3fed6
TalanBell <harpocrat@hotmail.com>
2020-12-09 15:32:02 +0000

    Showing a custom editor window, and opening an asset callback

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Callbacks;
 
 namespace RPG.Dialogue.Editor
 {
@@ -11,6 +12,18 @@ namespace RPG.Dialogue.Editor
         public static void ShowEditorWindow()
         {
             GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+        }
+
+        [OnOpenAssetAttribute(1)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+            if (dialogue != null)
+            {
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
         }
     }
 }
