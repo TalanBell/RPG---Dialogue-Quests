51e6dcad4316952fdcf95c322eefbce360e4601c
TalanBell <harpocrat@hotmail.com>
2020-12-22 18:47:30 +0000

    Callback mechanisms

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -8,13 +8,15 @@ namespace RPG.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
+        Dialogue selectedDialogue = null;
+
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowEditorWindow()
         {
             GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
         }
 
-        [OnOpenAssetAttribute(1)]
+        [OnOpenAsset(1)]
         public static bool OnOpenAsset(int instanceID, int line)
         {
             Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
@@ -26,11 +28,31 @@ namespace RPG.Dialogue.Editor
             return false;
         }
 
+        private void OnEnable() 
+        {
+            Selection.selectionChanged += OnSelectionChanged;   // C# event. No () or fn called then
+        }
+
+        private void OnSelectionChanged() 
+        {
+            Dialogue newDialogue = Selection.activeObject as Dialogue;
+            if (newDialogue != null)
+            {
+                selectedDialogue = newDialogue;
+                Repaint();
+            }
+        }
+
         private void OnGUI() 
         {
-            EditorGUILayout.LabelField("Interesting Label", "Offset label?");
-            EditorGUILayout.LabelField("Another interesting label");
-            EditorGUILayout.LabelField("Guess what? A third!");
+            if (selectedDialogue == null)
+            {
+                EditorGUILayout.LabelField("No Dialogue selected");
+            }
+            else
+            {
+                EditorGUILayout.LabelField(selectedDialogue.name);
+            }
         }
     }
 }
